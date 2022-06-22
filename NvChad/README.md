c61bc1e507f7e8e89e0fa06590b665833f6f20c8
andryNoor <andry.noor@outlook.com>
2022-06-22 11:51:33 +0700

    Initial commit

---
+++ NvChad/README.md
@@ -0,0 +1,2 @@
+Location: ~/.config/nvim/lua/[custom]
+Hint: Create symlink of this directory ["custom"] to NvChad custom folder
---
+++ NvChad/custom/chadrc.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+-- make sure you maintain the structure of `core/default_config.lua` here,
+
+M.ui = {
+  theme = "tokyonight",
+  transparency = true,
+}
+
+return M
