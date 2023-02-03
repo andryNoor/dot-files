4e385f20210e13ced99bebe4e63bc7080b268736
andryNoor <andry.noor07@gmail.com>
2023-02-03 20:50:57 +0700

    lspconfig.lua: add elixirls and override default elixirls config

--- NvChad/custom/plugins/lspconfig.lua
+++ NvChad/custom/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { "html", "cssls", "emmet_ls", "clangd", "csharp_ls", "pyright", "dartls", "yamlls", "jsonls", "gopls", "sqls", "tsserver", "bashls" }
+local servers = { "html", "cssls", "emmet_ls", "clangd", "csharp_ls", "pyright", "dartls", "yamlls", "jsonls", "gopls", "sqls", "tsserver", "bashls", "elixirls" }
 -- local servers = { "html", "cssls", "emmet_ls", "clangd", "pyright", "yamlls", "jsonls", "gopls", "sqls", "tsserver", "bashls" }
 
 for _, lsp in ipairs(servers) do
@@ -12,3 +12,10 @@ for _, lsp in ipairs(servers) do
 		root_dir = vim.loop.cwd,
 	})
 end
+
+lspconfig.elixirls.setup {
+  cmd = { "/home/mnoor/.local/share/nvim/mason/bin/elixir-ls" },
+  root_dir = vim.loop.cwd,
+  -- root_dir = vim.loop.os_homedir,
+  -- root_dir = root_pattern("mix.exs", ".git"),
+}
