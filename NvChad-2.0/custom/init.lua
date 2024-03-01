97e525c27c7f0145d874c972435d575dfe0ce5e8
andryNoor <andry.noor@outlook.com>
2024-03-01 18:21:15 +0700

    custom/init.lua: change default terminal shell to pwsh or powershell

--- NvChad-2.0/custom/init.lua
+++ NvChad-2.0/custom/init.lua
@@ -8,3 +8,18 @@ autocmd("VimResized", {
 
 -- Neovide
 require("custom.neovide")
+
+-- Change default terminal shell to pwsh.exe
+local powershell_options = {
+  shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+  shellcmdflag =
+  "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
