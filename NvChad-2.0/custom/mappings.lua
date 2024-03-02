213965cddebece98c5a21adf2db516bf6a08d045
andryNoor <andry.noor@outlook.com>
2024-03-02 23:07:42 +0700

    mapping.lua: add mapping for themes and tubuflines

--- NvChad-2.0/custom/mappings.lua
+++ NvChad-2.0/custom/mappings.lua
@@ -6,10 +6,66 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
 }
 
--- more keybinds!
+M.theme = {
+  n = {
+    ["<leader>tt"] = {
+      function()
+        require("base46").toggle_theme()
+      end,
+      "toggle theme [dark|light]",
+    },
+
+    ["<leader>tr"] = {
+      function()
+        require("base46").toggle_transparency()
+      end,
+      "toggle transparency",
+    },
+  },
+  v = {
+    ["<leader>tt"] = {
+      function()
+        require("base46").toggle_theme()
+      end,
+      "toggle theme [dark|light]",
+    },
+
+    ["<leader>tr"] = {
+      function()
+        require("base46").toggle_transparency()
+      end,
+      "toggle transparency",
+    },
+  },
+}
+
+M.tabufline = {
+  n = {
+    ["<leader><tab>"] = {
+      function()
+        require("nvchad.tabufline").move_buf(1)
+      end,
+      "arraange buffer (move right)",
+    },
+
+    ["<leader><S-tab>"] = {
+      function()
+        require("nvchad.tabufline").move_buf(-1)
+      end,
+      "arraange buffer (move left)",
+    },
+
+    ["<leader>wc"] = {
+      function()
+        require("nvchad.tabufline").closeAllBufs()
+      end,
+      "close all buffer",
+    },
+  },
+}
 
 return M
