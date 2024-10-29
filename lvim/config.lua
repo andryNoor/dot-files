7866aa9c260e59a300bbd160da95a6fcfa6faac1
andryNoor <andry.noor@outlook.com>
2024-10-30 01:35:06 +0700

    lvim: config.lua: choose colorscheme based on current terminal in used [Windows Terminal | not] and based on background type ['dark' | 'light']

--- lvim/config.lua
+++ lvim/config.lua
@@ -320,8 +320,9 @@ lvim.plugins = {
 if not vim.g.neovide then
   local background = vim.o.background                   -- background: dark || light
   local bubbles_theme = require "lualine_bubbles_theme" -- lualine custom theme
+  local terminal = is_windows_terminal() and 'tokyonight-moon' or 'tokyonight-day'
 
-  local colorscheme = background == 'dark' and 'tokyonight-moon' or 'auto'
+  local colorscheme = background == 'dark' and terminal or 'auto'
   local lualine_theme = background == 'dark' and bubbles_theme or 'auto'
   local refresh_lualine = require "lvim.core.lualine".setup -- needed for reloading lualine
   -- [https://github.com/nvim-lualine/lualine.nvim/issues/310#issuecomment-899924926]
